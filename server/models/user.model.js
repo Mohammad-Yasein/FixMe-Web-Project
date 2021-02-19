53ead3f9de65c96eea4754202f7d74bdc0acd333
Mohammad-Yasein <myn.shanabli@gmail.com>
2021-02-19 13:54:38 +0200

    create user.model.js file

---
+++ server/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'FIRST NAME IS REQUIRED!'],
+      minLength: [2, 'FIRST NAME MUST BE AT LEAST TWO CHARACTERS LONG!'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'LAST NAME IS REQUIRED!'],
+      minLength: [2, 'LAST NAME MUST BE AT LEAST TWO CHARACTERS LONG!'],
+    },
+    email: {
+      type: String,
+      required: [true, 'EMAIL IS REQUIRED!'],
+      index: true,
+      unique: true,
+      validate: {
+        validator: value => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(value),
+        message: 'INVALID EMAIL FORMAT!',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'PASSWORD IS REQUIRED!'],
+      minLength: [8, 'PASSWORD MUST BE AT LEAST EIGHT CHARACTERS LONG!'],
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.plugin(uniqueValidator, { message: 'EMAIL ADDRESS IS IN USE!' });
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function (error, salt) {
+    if (error) return next(error);
+    bcrypt.hash(user.password, salt, function (error, hash) {
+      if (error) return next(error);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+module.exports = mongoose.model('User', UserSchema);
