a93e75de0c565cdcbdad2571329202c8132b4e6e
Ayat-Ayyad <ayyadayat89@gmail.com>
2021-02-24 16:48:33 +0200

    modified main files: index and App files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,11 @@ import { Router } from '@reach/router';
 import Header from './components/Header';
 import RegistrationForm from './components/RegistrationForm';
 import LoginForm from './components/LoginForm';
-import Footer from './components/Footer';
 import HomePage from './components/HomePage';
+import ArticlesListPage from './components/ArticlesListPage';
+import ArticlePage from './components/ArticlePage';
+import ProfilePage from './components/ProfilePage';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -15,6 +18,9 @@ const App = () => {
         <RegistrationForm path="/register" />
         <LoginForm path="/login" />
         <HomePage path="/" />
+        <ArticlesListPage path="/articles" />
+        <ArticlePage path="/articles/:id" />
+        <ProfilePage path="/profiles/:id" />
       </Router>
       <Footer />
     </>
