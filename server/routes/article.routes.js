c9a1bccb8a58ee1a5981182fa0f0188478329338
GitHub-MahmoudWaked <mahmoud_waked@hotmail.com>
2021-02-24 15:46:19 +0200

    articles routes added!

--- server/routes/article.routes.js
+++ server/routes/article.routes.js
@@ -2,8 +2,8 @@ const ArticleController = require('../controllers/article.controller');
 const { authenticate } = require('../config/jwt.config');
 
 module.exports = app => {
-  app.post('/api/articles', authenticate, ArticleController.addArticle);
+  app.post('/api/articles', ArticleController.addArticle);
   app.get('/api/articles', ArticleController.getAllArticles);
   app.get('/api/articles/:id', ArticleController.getArticleById);
-  app.put('/api/articles/:id', authenticate, ArticleController.addComment);
+  app.put('/api/articles/:id', ArticleController.addComment);
 };
